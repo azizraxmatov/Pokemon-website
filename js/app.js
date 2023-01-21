86f052e7c9308ddec4afbea9e40e69c194aa7028
azizraxmatov <azizraxmatov249@gmail.com>
2023-01-21 14:50:09 +0500

    add

--- js/app.js
+++ js/app.js
@@ -4,9 +4,6 @@ var elBtnSearch = document.querySelector('[data-btnsearch]')
 
 var elCards = document.querySelector('[data-cards]')
 
-function pokemonCards(evt){
-    
-
 for(var i = 0; i < pokemons.length; i++) {
     var pokemon = pokemons[i];
     // var elUrl = "http://www.serebii.net/pokemongo/pokemon/001.png"
@@ -39,7 +36,7 @@ for(var i = 0; i < pokemons.length; i++) {
     elP.classList.add("name2");
     elKg.classList.add("weight");
     elImg.classList.add("img");
-}
+
     
     
 }
